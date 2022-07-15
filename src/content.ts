8631691a0f0ae25fe8bd0bb5750f30e4f26854e3
moxak <mak.1_@outlook.com>
2022-07-16 01:03:59 +0900

    hot fix

--- src/content.ts
+++ src/content.ts
@@ -4,6 +4,7 @@ const SVG_ICON_OPEN = '<svg class="svg-icon" viewBox="0 0 20 20"><path fill="non
 const SVG_ICON_CLOSE = '<svg class="svg-icon" viewBox="0 0 20 20"><path fill="none" d="M8.388,10.049l4.76-4.873c0.303-0.31,0.297-0.804-0.012-1.105c-0.309-0.304-0.803-0.293-1.105,0.012L6.726,9.516c-0.303,0.31-0.296,0.805,0.012,1.105l5.433,5.307c0.152,0.148,0.35,0.223,0.547,0.223c0.203,0,0.406-0.08,0.559-0.236c0.303-0.309,0.295-0.803-0.012-1.104L8.388,10.049z"></path></svg>';
 
 function insertResizer(observer: MutationObserver) {
+    const isVCFloating = document.getElementById("voice-chat-container-floating") ? true : false;
     const sections = document.getElementsByTagName("section") as HTMLCollectionOf<HTMLElement>;
     const userAreaSection = sections[0] as HTMLElement;
     const headerSection = sections[1] as HTMLElement;
@@ -61,11 +62,10 @@ function insertResizer(observer: MutationObserver) {
             headerSection.insertBefore(buttonContainer, headerSection.firstChild);
         }
        
+    } 
+    if (isVCFloating) {
+        displayVoiceChat(userAreaSection);
     }
-
-    console.log("Inserted resizer");
-    displayVoiceChat(userAreaSection);
-    
 };
 
 function observeDOMRendering(observedNode: HTMLElement, config? : MutationObserverInit) {
